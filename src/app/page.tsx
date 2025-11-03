00849ac356e005d8abad9e09560c019f5b843503
Evan Yu <50347938+Badbird5907@users.noreply.github.com>
2025-11-03 16:53:28 -0500

    feat: add uoft webring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,6 +26,17 @@ export default function Home() {
           <SideRight isMobile />
         </div>
       </div>
+      <div className="flex items-center justify-center gap-2">
+        <a href='https://uoftwebring.com/redirect?nav=prev&id=25'>←</a>
+        <a href='https://uoftwebring.com' target='_blank'>
+            <img
+                src='https://uoftwebring.com/ring_logo.svg'
+                alt='UofT Webring'
+                className="w-6 h-auto"
+            />
+        </a>
+        <a href='https://uoftwebring.com/redirect?nav=next&id=25'>→</a>
+      </div>
     </main>
   );
 }
