4eb0474471a4481c19b217987f8efa982bc012bc
Evan Yu <50347938+Badbird5907@users.noreply.github.com>
2025-11-21 05:39:35 -0500

    fix: better navbar animation

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -152,9 +152,9 @@ export default function Navbar() {
                 return (
                   <motion.div
                     key={item.name}
-                    initial={{ opacity: 0, y: -20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.3, delay: 0.3 + index * 0.1 }}
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.1, delay: 0.2 }}
                   >
                     <Link
                       href={item.href}
